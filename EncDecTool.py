95184a3697a31d8c671ee80ec378606bee736e0b
AungThuMyint <aungthumyint.com@gmail.com>
2021-01-30 22:25:37 +0630

    Update

--- EncDecTool.py
+++ EncDecTool.py
@@ -5,6 +5,10 @@ from binascii import Error
 from cryptography import *
 import cryptography
 import binascii
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 def run(runfile):
 	with open(runfile,"r") as rnf:
@@ -13,8 +17,43 @@ def run(runfile):
 def key_generat():
     os.system("clear")
     print(Style.BRIGHT + Fore.YELLOW + """
-    [1] Generate Key Options""")
+    [1] Generate Key [Auto] Options""")
     key = Fernet.generate_key()
+    key_name = input("""
+    Enter Key Name : """)
+    file = open(key_name + ".key", "wb")
+    file.write(key)
+    file.close()
+    print("""
+    Key Generated Successfully""")
+    print("""
+    Output File Name >>> """ + key_name + ".key\n")
+    print(Style.RESET_ALL)
+    run("EncDecTool.py")
+
+def key_generat_manual():
+    print(Style.BRIGHT + Fore.CYAN + """
+    [2] Generate Key [Manual] Options""")
+    pass_from_user = input("""
+    Key Password : """)
+    password = pass_from_user.encode()
+
+    mysalt = b'q\xe3Q5\x8c\x19~\x17\xcb\x88\xc6A\xb8j\xb4\x85'
+
+    kdf = PBKDF2HMAC (
+        algorithm=hashes.SHA256,
+        length=32,
+        salt=mysalt,
+        iterations=100000,
+        backend=default_backend()
+
+    )
+
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+
+    print("""
+    Your Key Is : """ + key.decode())
+    my_key = key.decode()
     key_name = input("""
     Enter Key Name : """)
     file = open(key_name + ".key", "wb")
@@ -29,7 +68,7 @@ def key_generat():
 
 def encrypt_file():
     os.system("clear")
-    print(Style.BRIGHT + Fore.MAGENTA + """
+    print(Style.BRIGHT + Fore.GREEN + """
     [2] Encrypt File Options""")
     try:
         key_file = input("""
@@ -61,7 +100,7 @@ def encrypt_file():
 
 def decrypt_file():
     os.system("clear")
-    print(Style.BRIGHT + Fore.GREEN + """
+    print(Style.BRIGHT + Fore.CYAN + """
     [3] Decrypt File Options""")
 
     try:    
@@ -100,15 +139,17 @@ print(Style.BRIGHT + Fore.GREEN + """
      / _// _ \/ __/ __/ // / _ \/ __/  > _/_ _/ / // / -_) __/ __/ // / _ \/ __/ / _// / / -_)
     /___/_//_/\__/_/  \_, / .__/\__/  |_____/  /____/\__/\__/_/  \_, / .__/\__/ /_/ /_/_/\__/ 
                     /___/_/                                    /___/_/                       
-                                Code By : Aung Thu Myint""" + Style.BRIGHT + Fore.CYAN + """
+                                """ + Style.BRIGHT + Fore.RED + "Coder By : Aung Thu Myint" + Style.BRIGHT + Fore.CYAN + """
 
-        [1] Generate Key
+        [1] Generate Key [Auto]
+
+        [2] Generate Key [Manual]
         
-        [2] Encrypt File
+        [3] Encrypt File
         
-        [3] Decrypt File
+        [4] Decrypt File
         
-        [4] Exit""" + Style.RESET_ALL)
+        [5] Exit""" + Style.RESET_ALL)
 
 print(Style.BRIGHT + Fore.BLUE)
 enter_options = str(input("""
@@ -118,12 +159,15 @@ if enter_options==str(1):
     key_generat()
 
 elif enter_options==str(2):
-    encrypt_file()
+    key_generat_manual()
 
 elif enter_options==str(3):
-    decrypt_file()
+    encrypt_file()
 
 elif enter_options==str(4):
+    decrypt_file()
+
+elif enter_options==str(5):
     print(Style.BRIGHT + Fore.RED + """
     Bye Bye ... !\n""" + Style.RESET_ALL)
     exit()
